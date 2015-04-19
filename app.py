d1b4c53382bdff7bc12fa7df6bb4076e8d1e146b
Dan Kolbman <dankolbman@gmail.com>
2015-04-19 04:10:33 -0400

    Add text responses

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, jsonify
-import random
+from flask import Flask, render_template, jsonify, flash, request
+from flask.ext.wtf import Form
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import Required, Length, Email
+import random, twilio.twiml
+
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = 'G00DB33F'
 
 views = 0
 
@@ -9,21 +14,44 @@ views = 0
 def index():
   global views
   views += 1
-  facts = open('facts.txt').read().splitlines()
-  fact =random.choice(facts)
+  fact = rand_fact()
 
   return render_template('index.html', fact=fact, views=views)
 
 @app.route("/api")
 def api():
-  print('hi')
-  facts = open('facts.txt').read().splitlines()
-  fact =random.choice(facts)
-  return jsonify({ 'fact' : fact })
+  return jsonify({ 'fact' : rand_fact() })
 
 @app.route("/apiman")
 def apiman():
   return render_template('api.html')
+
+@app.route("/incoming", methods=['GET', 'POST'])
+def respond():
+  """ Responds with a fact """
+  from_number = request.values.get('From', None)
+  message = 'Brick Fact: '
+  message += rand_fact()
+
+  resp = twilio.twiml.Response()
+  resp.message(message)
+  return str(resp)
+
+
+def rand_fact():
+  """ The crown jewels!!!! """
+  facts = open('facts.txt').read().splitlines()
+  fact = random.choice(facts)
+  return fact
+
+
+###### FORMS #####
+
+class PhoneForm(Form):
+  phone_num = IntegerField('Phone Number', validators = [Required(), Length(9,10)])
+  submit = SubmitField('Submit')
+
+
 
 if __name__ == "__main__":
   app.run()
