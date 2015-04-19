aad651fe44b09f2606198fb8e74d5e33f7756d67
Dan Kolbman <dankolbman@gmail.com>
2015-04-19 00:16:28 -0400

    Add api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import random
 app = Flask(__name__)
 app.debug = True
@@ -7,12 +7,20 @@ views = 0
 
 @app.route("/")
 def index():
-    global views
-    views += 1
-    facts = open('facts.txt').read().splitlines()
-    fact =random.choice(facts)
+  global views
+  views += 1
+  facts = open('facts.txt').read().splitlines()
+  fact =random.choice(facts)
 
-    return render_template('index.html', fact=fact, views=views)
+  return render_template('index.html', fact=fact, views=views)
+
+@app.route("/api")
+def api():
+  print('hi')
+  facts = open('facts.txt').read().splitlines()
+  fact =random.choice(facts)
+  return jsonify({ 'fact' : fact })
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
+
